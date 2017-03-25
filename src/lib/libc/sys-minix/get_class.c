d77bdbd14ca914f562570a4f5b4fdd27b2ae0c25
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-03-25 17:50:32 -0400

    Added prints to get_class

--- src/lib/libc/sys-minix/get_class.c
+++ src/lib/libc/sys-minix/get_class.c
@@ -6,12 +6,14 @@ int get_class(FILE* fd){
 	if(fd == NULL || fileno(fd)==-1){
 		errno=1;
 		return -1;
+		printf("GET_CLASS ERROR\n");
 	}
 
 	message m;
 	m.m1_i1=fileno(fd);
 	m.m1_i2=0;
 	m.m1_i3=0;
+	printf("GET_CLASS NO ERROR\n");
 
 	return _syscall(VFS_PROC_NR, CLASS, &m);
 }
