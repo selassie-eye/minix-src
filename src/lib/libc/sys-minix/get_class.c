f45c7f6030a359f1ae02424f7ef6156615b23994
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-03-26 00:42:58 -0400

    testing conditionals

--- src/lib/libc/sys-minix/get_class.c
+++ src/lib/libc/sys-minix/get_class.c
@@ -3,17 +3,15 @@
 #include <stdio.h>
 #include <errno.h>
 int get_class(FILE* fd){
-	if(fd == NULL || fileno(fd)==-1){
+	if(fd == NULL){
 		errno=1;
 		return -1;
-		printf("GET_CLASS ERROR\n");
 	}
 
 	message m;
 	m.m1_i1=fileno(fd);
 	m.m1_i2=0;
 	m.m1_i3=0;
-	printf("GET_CLASS NO ERROR\n");
 
 	return _syscall(VFS_PROC_NR, CLASS, &m);
 }
