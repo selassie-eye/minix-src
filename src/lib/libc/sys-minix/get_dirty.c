4b33c64ce7724c963c3945dce7114b14f94804a1
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-03-30 13:31:40 -0400

    fixed get_dirty typo

--- src/lib/libc/sys-minix/get_dirty.c
+++ src/lib/libc/sys-minix/get_dirty.c
@@ -19,6 +19,6 @@ int get_dirty(FILE* fd, int newvalue){
 		m.m1_i1 = fileno(fd);
 		m.m1_i2 = newvalue;
 		m.m1_i3=2;
-		return _syscall(VFS_PROC_NR, DO_CLASS, &m);
+		return _syscall(VFS_PROC_NR, CLASS, &m);
 	}
 }
